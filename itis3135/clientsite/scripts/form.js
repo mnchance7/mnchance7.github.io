034432bdbbeccb3197ba7f6603edd24768267292
mnchance7 <mchance7@uncc.edu>
2024-05-05 17:17:41 -0400

    form.js

---
+++ itis3135/clientsite/scripts/form.js
@@ -0,0 +1,5 @@
+document.getElementById('questionForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your questions! We\'ll get back to you as soon as possible.');
+    
+});
